6b629b060f66dd904a67dd25b4c369439e65d2b1
Jamie Brown <jmb.black3nd@gmail.com>
2021-01-17 11:12:27 +0200

    feat(main): create function to return the square root of a chosen number

---
+++ main.py
@@ -0,0 +1,9 @@
+# function to get the square root of a chosen number
+def get_square_root_of_number(number):
+    return number ** 1/2
+
+
+# run from main
+if __name__ == "__main__":
+    # get the square root of 16
+    get_square_root_of_number(16)
